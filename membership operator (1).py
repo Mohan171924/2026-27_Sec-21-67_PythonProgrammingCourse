4541e7598044bbdcb4f34786c81d1d910fc689f2
Mohan <mohan181924@gmail.com>
2026-09-04 14:39:13 +0530

    Add files via upload

---
+++ membership operator (1).py
@@ -0,0 +1,24 @@
+#memebership operator exercise
+'m' in 'mango'
+print("result is:",'m' in 'mango' )
+
+'h' in 'mango'
+print("result is:",'h' in 'mango')
+
+69 in [ 1,2,3,4,5]
+print("result is:",69 in [1,2,3,4,5])
+
+67 in [ 76,67,68,69]
+print("result is:",67 in [ 76,67,68,69])
+
+'k' not in 'krishna'
+print("result is:",'k' in 'krishna')
+
+'r' not in 'krishna'
+print("result is:",'r' not in 'krishna')
+
+67 not in [ 76,67,68,69]
+print("result is:",67 not in [ 76,67,68,69])
+
+7899 not in [1,2,3,4,5,6]
+print("result is:",7899 not in [1,2,3,4,5,6])
