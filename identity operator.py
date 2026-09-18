21fde3a8c51658bb8ff362af7a0a7887addc92e5
Mohan <mohan181924@gmail.com>
2026-09-18 13:53:16 +0530

    Refactor identity operator exercise
    Refactor identity operator exercise to include comments and print statements for clarity.

--- identity operator.py
+++ identity operator.py
@@ -1,12 +1,11 @@
-#python identity operator exercise
-#a =[1, 2, 3, 4]
-#b = a
-#print("a is b:", a is b)
+a =[1, 2, 3, 4]
+b = a
+print("a is b:", a is b)
 
 a= [1, 2, 3, 4]
 b= [1, 2, 3, 4]
 print("a is b:", a is b)
 c=[1,3,4,7,]
 d=[1,3,4,7]
-result=d is c # is ideentity operator
+result=d is c # is identity operator
 print("result of",d,"is")
